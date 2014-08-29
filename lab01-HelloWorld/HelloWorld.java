00c61b64b659ac2a7581a58d1f8275a7909a6dac
William Schwarz <was316@lehigh.edu>
2014-08-29 14:47:01 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,30 @@
+///////////////////////////////////////////////////////////////////////////////////////
+//Will Schwarz
+//Lab 1
+//Hello World Java Program
+
+//  compile program
+//      javac HelloWorld.java
+//  run the program
+//  java HelloWorld//
+
+//  define a class
+public class HelloWorld{
+    
+//  add main method
+    public static void main(String[] args)  {
+        
+        //print the statement with box around it
+        System.out.println("#########################");
+        System.out.println("#########################");
+        System.out.println("#                       #");
+        System.out.println("#                       #");
+        System.out.println("#   Hello World 2014!!  #");
+        System.out.println("#                       #");
+        System.out.println("#########################");
+        System.out.println("#########################");
+        System.out.println("test");
+        
+            //test/testtest
+    }
+}
