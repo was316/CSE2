9bb957e2a0e822df03ffa56555667215673e9ac4
William Schwarz <was316@lehigh.edu>
2014-09-02 01:02:59 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,28 @@
+///////////////////////////////////////
+//William Schwarz
+//hw01
+//Welcome Class Java Program
+
+
+
+//define a class
+public class WelcomeClass{
+    
+    //add main method
+    public static void main(String[] args)   {
+        
+        //print the statement
+        System.out.println("------------");
+        System.out.println("|  WELCOME |");
+        System.out.println("------------");
+        System.out.println("  ^  ^  ^  ^  ^  ^ ");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("|-W--A--S--3--1--6-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("Hi, my name is Will. I like longs walks on the");
+        System.out.println("beach, casual games of croquet, and fine cigars");
+        
+        
+    }
+}
