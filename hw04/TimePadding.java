82159e23dadcfe369050882e4fea35315627e0ae
William Schwarz <was316@lehigh.edu>
2014-09-24 00:40:24 +0000

    updated TimePadding

--- hw04/TimePadding.java
+++ hw04/TimePadding.java
@@ -18,3 +18,26 @@ import java.util.Scanner;
             
                 //prompt the user for thier income
                 System.out.print("Enter the time in seconds : ");
+                
+                    //accept input
+                    int time= myScanner.nextInt();
+                    //convert input to broken down time
+                    int hours= time/3600;
+                    int minutes= (time- (3600*hours))/60;
+                    int seconds= (time- (3600*hours))-minutes*60;
+                    
+                        //if statements to display the padded time
+                        if(minutes>9&&seconds>9){
+                            System.out.println("the time is - "+hours+" : "+minutes+" : "+seconds);
+                        }
+                        else if(minutes<10&&seconds>9){
+                            System.out.println("the time is - "+hours+" : 0"+minutes+" : "+seconds);
+                        }
+                        else if(minutes>9&&seconds<10){
+                            System.out.println("the time is - "+hours+" : "+minutes+" : 0"+seconds);
+                        }
+                        else if(minutes<10&&seconds<10){
+                            System.out.println("the time is - "+hours+" : 0"+minutes+" : 0"+seconds);
+                        }
+        }
+    }
